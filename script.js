ffd8fe414892c0adc10cb5a9cad9c3a53f39a20f
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-01-20 18:17:54 +0530

    feat - drawer component

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const hideBtn = document.querySelector('.button-toggle-badge')
 const toggleBadge = document.querySelector('.toggle-badge')
 
-hideBtn.addEventListener('click', (e) => {
+hideBtn.addEventListener('click', () => {
     if(toggleBadge.style.display == 'none')
     toggleBadge.style.display = 'block'
     else toggleBadge.style.display = 'none'
