e9d370a88b1d6b7b3e80eacce115aa3cc73ee813
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-01-20 14:33:28 +0530

    feat - badge component

---
+++ script.js
@@ -0,0 +1,8 @@
+const hideBtn = document.querySelector('.button-toggle-badge')
+const toggleBadge = document.querySelector('.toggle-badge')
+
+hideBtn.addEventListener('click', (e) => {
+    if(toggleBadge.style.display == 'none')
+    toggleBadge.style.display = 'block'
+    else toggleBadge.style.display = 'none'
+})
