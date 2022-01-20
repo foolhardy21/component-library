f5d93adf325a1dd799f6a9eab041443c379a9d53
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-01-20 18:29:30 +0530

    feat - drawer active item color change update

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const hideBtn = document.querySelector('.button-toggle-badge')
 const toggleBadge = document.querySelector('.toggle-badge')
+const drawerItems = document.querySelectorAll('.drawer-menu-item')
 
 hideBtn.addEventListener('click', () => {
     if(toggleBadge.style.display == 'none')
     toggleBadge.style.display = 'block'
     else toggleBadge.style.display = 'none'
+})
+
+drawerItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        drawerItems.forEach(item2 => {
+            item2.classList.remove('drawer-menu-item-active')    
+        })
+        item.classList.add('drawer-menu-item-active')
+    })
+    
 })
