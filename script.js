63484f3a62ea1efa0360d60fae4cdc8ca8de4c3d
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-01-21 19:37:52 +0530

    feat - tabs component

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-const hideBtn = document.querySelector('.button-toggle-badge')
-const toggleBadge = document.querySelector('.toggle-badge')
+const hideBadgeBtn = document.querySelector('.button-toggle-badge')
+const badge = document.querySelector('.toggle-badge')
+
 const drawerItems = document.querySelectorAll('.drawer-menu-item')
+
 const openDialogBtn = document.querySelector('.open-dialog')
 const alertDiv = document.querySelector('.dialog')
-const dialogAction = document.querySelectorAll('.dialog-action-btn')
+const dialogActionBtn = document.querySelectorAll('.dialog-action-btn')
 
-hideBtn.addEventListener('click', () => {
-    if(toggleBadge.style.display == 'none')
-    toggleBadge.style.display = 'block'
-    else toggleBadge.style.display = 'none'
+const tabItems = document.querySelectorAll('.tab-item')
+const leadIconItems = document.querySelectorAll('.tabitem-lead-icon')
+const topIconItems = document.querySelectorAll('.tabitem-top-icon')
+
+hideBadgeBtn.addEventListener('click', () => {
+    if(badge.style.display == 'none')
+    badge.style.display = 'block'
+    else badge.style.display = 'none'
 })
 
 drawerItems.forEach((item) => {
@@ -24,8 +30,38 @@ openDialogBtn.addEventListener('click', () => {
     alertDiv.style.display = 'block'
 })
 
-dialogAction.forEach(actionBtn => {
+dialogActionBtn.forEach(actionBtn => {
     actionBtn.addEventListener('click', () => {
         alertDiv.style.display = 'none'
+    })
+})
+
+tabItems.forEach(item => {
+    item.addEventListener('click', () => {
+        tabItems.forEach(item2 => {
+            item2.classList.remove('tab-item-selected')
+        })
+        item.classList.add('tab-item-selected')
+    })
+})
+
+// console.log(tabItems)
+leadIconItems.forEach(item => {
+    item.addEventListener('click', () => {
+        // console.log('clicked')
+        leadIconItems.forEach(item2 => {
+            item2.classList.remove('tab-item-selected')
+        })
+        item.classList.add('tab-item-selected')
+    })
+})
+
+topIconItems.forEach(item => {
+    item.addEventListener('click', () => {
+        // console.log('clicked')
+        topIconItems.forEach(item2 => {
+            item2.classList.remove('tab-item-selected')
+        })
+        item.classList.add('tab-item-selected')
     })
 })
