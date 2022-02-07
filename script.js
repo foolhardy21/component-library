e7770db3b47f64aa37a0c1816388c497a1a28d38
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-02-07 20:01:04 +0530

    feat - lists added

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const formSubmit = document.querySelector('#submit-btn')
 const formInput = document.querySelector('#input')
+const drawerItems = document.querySelectorAll('.list-menu-item')
+
+drawerItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        drawerItems.forEach(item2 => {
+            item2.classList.remove('list-menu-item-active')    
+        })
+        item.classList.add('list-menu-item-active')
+    })  
+})
 
 formSubmit.addEventListener('click', () => {
     formInput.value= ''
